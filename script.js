29b18a58af865f1edf48200cac231b65f0571971
crkos <jordandark123@gmail.com>
2021-10-26 19:32:58 -0600

    master

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () =>{
+    return Math.round((Math.random() * 9));
+
+}
+
+const compareGuesses = (currentHumanGuess, computerGuess, target) => {
+    if (Math.abs(currentHumanGuess - target) < Math.abs(computerGuess - target)) {
+        return true;
+    } else if (Math.abs(computerGuess - target) < Math.abs(currentHumanGuess - target)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+const updateScore = winner => {
+    if(winner === 'human'){
+        humanScore++;
+    }
+    else{
+        computerScore++;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
+
 
